36f314a3f2e3d79dd260f4e41becd97bf6c571a3
ryancocuzzo <rcocuzzo8@gmail.com>
2026-08-17 05:40:54 -0400

    Fix SignalWithStart livelock on orphaned current-execution pointer

---
+++ tests/signal_with_start_orphan_pointer_test.go
@@ -0,0 +1,109 @@
+package tests
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+	commandpb "go.temporal.io/api/command/v1"
+	enumspb "go.temporal.io/api/enums/v1"
+	"go.temporal.io/api/workflowservice/v1"
+	"go.temporal.io/server/chasm"
+	"go.temporal.io/server/common"
+	"go.temporal.io/server/common/persistence"
+	"go.temporal.io/server/common/testing/parallelsuite"
+	"go.temporal.io/server/common/testing/taskpoller"
+	"go.temporal.io/server/common/testing/testvars"
+	"go.temporal.io/server/tests/testcore"
+)
+
+type SignalWithStartOrphanPointerSuite struct {
+	parallelsuite.Suite[*SignalWithStartOrphanPointerSuite]
+}
+
+func TestSignalWithStartOrphanPointerSuite(t *testing.T) {
+	parallelsuite.Run(t, &SignalWithStartOrphanPointerSuite{})
+}
+
+func (s *SignalWithStartOrphanPointerSuite) TestSignalWithStartAfterOrphanedCompletedPointer() {
+	env := testcore.NewEnv(s.T(), testcore.WithDedicatedCluster())
+	tv := testvars.New(s.T())
+	ctx := s.Context()
+
+	shardID := common.WorkflowIDToHistoryShard(
+		env.NamespaceID().String(),
+		tv.WorkflowID(),
+		env.GetTestClusterConfig().HistoryConfig.NumHistoryShards,
+	)
+	execMgr := env.GetTestCluster().ExecutionManager()
+	poller := taskpoller.New(s.T(), env.FrontendClient(), env.Namespace().String())
+
+	startResp, err := env.FrontendClient().StartWorkflowExecution(ctx, &workflowservice.StartWorkflowExecutionRequest{
+		RequestId:    uuid.NewString(),
+		Namespace:    env.Namespace().String(),
+		WorkflowId:   tv.WorkflowID(),
+		WorkflowType: tv.WorkflowType(),
+		TaskQueue:    tv.TaskQueue(),
+	})
+	s.Require().NoError(err)
+	orphanedRunID := startResp.RunId
+
+	_, err = poller.PollAndHandleWorkflowTask(tv, func(_ *workflowservice.PollWorkflowTaskQueueResponse) (*workflowservice.RespondWorkflowTaskCompletedRequest, error) {
+		return &workflowservice.RespondWorkflowTaskCompletedRequest{
+			Commands: []*commandpb.Command{{
+				CommandType: enumspb.COMMAND_TYPE_COMPLETE_WORKFLOW_EXECUTION,
+				Attributes: &commandpb.Command_CompleteWorkflowExecutionCommandAttributes{
+					CompleteWorkflowExecutionCommandAttributes: &commandpb.CompleteWorkflowExecutionCommandAttributes{},
+				},
+			}},
+		}, nil
+	})
+	s.Require().NoError(err)
+
+	s.Require().NoError(execMgr.DeleteWorkflowExecution(ctx, &persistence.DeleteWorkflowExecutionRequest{
+		ShardID:     shardID,
+		NamespaceID: env.NamespaceID().String(),
+		WorkflowID:  tv.WorkflowID(),
+		RunID:       orphanedRunID,
+		ArchetypeID: chasm.WorkflowArchetypeID,
+	}))
+	waitForMutableStateGone(ctx, env, shardID, execMgr, tv.WorkflowID(), orphanedRunID)
+
+	current, err := execMgr.GetCurrentExecution(ctx, &persistence.GetCurrentExecutionRequest{
+		ShardID:     shardID,
+		NamespaceID: env.NamespaceID().String(),
+		WorkflowID:  tv.WorkflowID(),
+		ArchetypeID: chasm.WorkflowArchetypeID,
+	})
+	s.Require().NoError(err)
+	s.Require().Equal(orphanedRunID, current.RunID)
+	s.Require().Equal(enumspb.WORKFLOW_EXECUTION_STATUS_COMPLETED, current.Status)
+
+	env.CloseShard(env.NamespaceID().String(), tv.WorkflowID())
+
+	swsCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+	swsResp, err := env.FrontendClient().SignalWithStartWorkflowExecution(swsCtx, &workflowservice.SignalWithStartWorkflowExecutionRequest{
+		RequestId:             uuid.NewString(),
+		Namespace:             env.Namespace().String(),
+		WorkflowId:            tv.WorkflowID(),
+		WorkflowType:          tv.WorkflowType(),
+		TaskQueue:             tv.TaskQueue(),
+		Identity:              "orphan-pointer-test",
+		SignalName:            "repro",
+		WorkflowIdReusePolicy: enumspb.WORKFLOW_ID_REUSE_POLICY_ALLOW_DUPLICATE,
+	})
+	s.Require().NoError(err)
+	s.Require().True(swsResp.Started)
+	s.Require().NotEqual(orphanedRunID, swsResp.RunId)
+
+	replaced, err := execMgr.GetCurrentExecution(ctx, &persistence.GetCurrentExecutionRequest{
+		ShardID:     shardID,
+		NamespaceID: env.NamespaceID().String(),
+		WorkflowID:  tv.WorkflowID(),
+		ArchetypeID: chasm.WorkflowArchetypeID,
+	})
+	s.Require().NoError(err)
+	s.Require().Equal(swsResp.RunId, replaced.RunID)
+}
